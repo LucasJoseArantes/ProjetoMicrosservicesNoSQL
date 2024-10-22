26451a7e7933d7c01385e2f331a330a207802458
Lucas José Arantes Silva <132081624+LucasJoseArantes@users.noreply.github.com>
2024-10-21 21:58:19 -0300

    UPDATE: Create and Delete User

--- user-api/src/main/java/com/example/user_api/controller/UserController.java
+++ user-api/src/main/java/com/example/user_api/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.example.user_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.user_api.dto.UserDTO;
+
 import jakarta.annotation.PostConstruct;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +37,15 @@ public class UserController {
         userDTO.setPhone("1234-3454");
         userDTO.setSignUpDate(LocalDateTime.now());
         users.add(userDTO);
+
+    UserDTO anotherUserDTO = new UserDTO();
+    anotherUserDTO.setName("Maria");
+    anotherUserDTO.setCpf("456");
+    anotherUserDTO.setAdress("Rua b");
+    anotherUserDTO.setEmail("maria@email.com");
+    anotherUserDTO.setPhone("5678-9101");
+    anotherUserDTO.setSignUpDate(LocalDateTime.now());
+    users.add(anotherUserDTO);
     }
 
     @GetMapping
@@ -40,4 +60,18 @@ public class UserController {
             .findFirst()
             .orElseThrow(() -> new RuntimeException("User not found"));
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO createUser(@Valid @RequestBody UserDTO userDTO) {
+        userDTO.setSignUpDate(LocalDateTime.now());
+        users.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean deleteUser(@PathVariable String cpf) {
+        return users.removeIf(user -> user.getCpf().equals(cpf));
+    }
+
 }
